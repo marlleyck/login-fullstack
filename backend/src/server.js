0d5ca1c1e5f650fbd9e71fa0ae8e06acf54d67c6
marlleyck <marlleyck@hotmail.com>
2022-12-03 00:42:56 -0300

    Initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.listen(8080, () => console.log('Server is running...'))
