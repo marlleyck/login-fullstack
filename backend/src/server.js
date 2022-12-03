1b26410dca541fadaa420098863b6a04a4690c45
marlleyck <marlleyck@hotmail.com>
2022-12-03 01:02:11 -0300

    feat: add initialize server

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const cors = require('cors')
 
+const router = require('./routes')
+
 const app = express()
 
+// Express configs
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(router)
 
 app.listen(8080, () => console.log('Server is running...'))
