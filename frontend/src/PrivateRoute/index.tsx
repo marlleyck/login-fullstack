29c9ecadb0ae985244e5eb99cb64e89b729c72a4
marlleyck <marlleyck@hotmail.com>
2022-12-03 02:32:35 -0300

    feat: add private route

---
+++ frontend/src/PrivateRoute/index.tsx
@@ -0,0 +1,11 @@
+import { useContext } from "react"
+import { AppContext } from "../contexts/AppContext"
+
+import { Navigate } from "react-router-dom"
+
+export const PrivateRoute = ({children}: {children: JSX.Element}) => {
+    const { authenticated } = useContext(AppContext)
+    
+    return authenticated ? children : <Navigate to='/' />
+    
+}
